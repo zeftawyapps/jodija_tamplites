38b5f75ef9f2094af43cb0c177d1fd62139c3778
HP <moaz.salaheddin>
2024-09-04 07:08:31 +0300

    d

--- lib/theams/colors/colors.dart
+++ lib/theams/colors/colors.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
 
-  Color primary() ;
-  Color secondary() ;
-  Color accent() ;
-  Color background() ;
+  Color primary = Color(0xE4BB0404)   ;
+  Color secondary = Color(0xFF564D4D);
+  Color accent  = Color(0xFFF1EAE5);
+  Color background  = Color(0xD3DFE0ED);
 
   // fonts text color
-  Color titleText() ;
-  Color subTitleText() ;
-  Color bodyText() ;
-  Color captionText() ;
-  Color buttonText() ;
+  Color titleText  = Color(0xFF010315);
+  Color subTitleText  = Color(0xFF2F2F32);
+  Color bodyText    = Color(0xFF5C5959);
+  Color captionText   = Color(0xFF3F3F40);
+  Color buttonText    = Color(0xFFE9EBF2);
   // controls and widgets
-  Color buttonColor() ;
-  Color iconColor() ;
-  Color dividerColor() ;
+  Color buttonColor  = Color(0xFF2531D3);
+  Color iconColor  = Color(0xE4BB0404);
+  Color dividerColor  = Color(0xFF1A1A2B);
 // other colors
 
 
