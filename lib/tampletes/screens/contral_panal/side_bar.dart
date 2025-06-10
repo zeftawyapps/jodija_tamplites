1c867b4b35f379ead8a7ef4b9fea77b4969c0682
HP <moaz.salaheddin>
2025-06-10 22:29:03 +0300

    dd

--- lib/tampletes/screens/contral_panal/side_bar.dart
+++ lib/tampletes/screens/contral_panal/side_bar.dart
@@ -6,36 +6,29 @@ import 'package:provider/provider.dart';
 
 import 'side_bar_tools/side_bar_widget.dart';
 
-
 class SideBar extends StatelessWidget {
-    SideBar({super.key});
+  SideBar({super.key});
 
   DashboardMainServices logic = DashboardMainServices();
 
   @override
   Widget build(BuildContext context) {
-    logic =  Provider.of<DashboardMainServices>(context);
+    logic = Provider.of<DashboardMainServices>(context);
 
-
-
-    return logic.logo == "Default Logo" ?Container() : Container(
-      width: 200  ,
-      color:   logic.sideBarColor ,
+    return Container(
+      width: 200,
+      color: logic.sideBarColor,
       child: Column(
         children: [
-          Container(
-              height: 200 ,
-              width: 200 ,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(logic.logo),
-                fit: BoxFit.fill
-              )
-            ),
-
-
-
-          ),
+          logic.logo == "Default Logo"
+              ? Container()
+              : Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(logic.logo), fit: BoxFit.fill)),
+                ),
           ...logic.getSideBar()
         ],
       ),
