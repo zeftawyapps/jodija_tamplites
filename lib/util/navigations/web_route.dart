d736847115e04aedf3f541fdb4bdaf8b8b68d301
HP <moaz.salaheddin>
2025-06-11 08:25:20 +0300

    web url

--- lib/util/navigations/web_route.dart
+++ lib/util/navigations/web_route.dart
@@ -1,8 +1,23 @@
+import 'dart:html' as html;
 
-
-class WebRouteUrlSplitter  {
- Map<String , dynamic> query = {};
-  Map<String , String> pram = {};
-  String url  = "";
+class WebRouteUrlSplitter {
+  Map<String, dynamic> query = {};
+  Map<String, String> pram = {};
+  String url = "";
   String routeName = "";
+}
+
+class WebRoute {
+  static void updateUrl(String path) {
+    html.window.history.pushState(null, '', path);
+  }
+
+  static void updateUrlWithQuery(String path, Map<String, dynamic> query) {
+    String queryString = Uri(queryParameters: query).query;
+    html.window.history.pushState(null, '', '$path?$queryString');
+  }
+
+  static String getCurrentPath() {
+    return html.window.location.pathname!;
+  }
 }
