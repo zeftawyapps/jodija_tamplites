e7c31202c6c8ab681cd70dc34f1cb0f03516b0ac
HP <moaz.salaheddin>
2025-06-15 20:58:51 +0300

    d

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=G:\mobile1\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\z_apps\tools\jodija-flutter\JoDija_tamplites_module"
+export "FLUTTER_APPLICATION_PATH=F:\z_apps\tools\jodija-flutter\jodija_tamplites"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
